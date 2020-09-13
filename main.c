5076542183e835587d6c4384d576477b5820ec23
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2020-09-13 14:28:50 +1200

    Add comments and tidy code

--- main.c
+++ main.c
@@ -1,15 +1,40 @@
+/*
+ * COSC242 Assignment 2020
+ * Authors: Maaha Ahmad, Magdeline Huang, Kate Truman
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "mylib.h"
-#include "tree.h"
 #include <getopt.h>
 #include <time.h>
+#include "mylib.h"
+#include "tree.h"
 
-static void print_info(int freq, char *word){
+/* 
+ * Prints the frequencies of the words
+ *
+ * @param freq: The number of times the word appears
+ * @param word: The word of interest
+ */
+static void print_info(int freq, char *word) {
     printf("%-4d %s\n", freq, word);
 }
 
+/*
+ * Processes two groups of words and effectively performs a basic spell check.
+ * The first group of words will be read from stdin and will be inserted into the tree.
+ * The second group of words will be read from a file specified on the command line.
+ * If any word read from the file is not contained in the tree then it should get printed to stdout.
+ * This program is implemented as a combination tree ADT which can be either an
+ * ordinary binary search tree or a balanced red-black tree.
+ * This function will deallocate all memory before finishing. 
+ *
+ * @param argc: The number of command line arguments
+ * @param argv: An array of strings that holds the command line arguments
+ *
+ * @return: The exit status of the program, either EXIT_SUCCESS or EXIT_FALIURE
+ */
 int main(int argc, char **argv) {
     const char *outputFilename = "tree-view.dot";
     const char *inputFilename = NULL;
@@ -22,14 +47,12 @@ int main(int argc, char **argv) {
     int unknownWords = 0;
     FILE *document = NULL;
     tree t;
-    /*timing info */
     clock_t fill_start, fill_end, search_start, search_end;
     double fill_time = 0.0;
     double search_time = 0.0;
-    /* end timing info */
     
-    while ((option = getopt(argc, argv, optstring)) != EOF){
-        switch(option){
+    while ((option = getopt(argc, argv, optstring)) != EOF) {
+        switch(option) {
             case 'f':
                 outputFilename = optarg;
                 break;
@@ -53,38 +76,38 @@ int main(int argc, char **argv) {
                 break;
             case 'h':
             default:
-                 fprintf(stderr, "Usage: ./%s [OPTION]... <STDIN>\n", argv[0]);
-                 fprintf(stderr, "\n");
-                 fprintf(stderr, "Perform various operations using a binary tree.  By default, words\n");
-                 fprintf(stderr, "are read from stdin and added to the tree, before being printed out\n");
-                 fprintf(stderr, "alongside their frequencies to stdout.\n");
-                 fprintf(stderr, "\n");
-                 fprintf(stderr, "-c FILENAME  Check spelling of words in FILENAME using words\n");
-                 fprintf(stderr, "             read from stdin as the dictionary.  Print timing\n");
-                 fprintf(stderr, "             info & unknown words to stderr (ignore -d & -o)\n");
-                 fprintf(stderr, "-d           Only print the tree depth (ignore -o)\n");
-                 fprintf(stderr, "-f FILENAME  Write DOT output to FILENAME (if -o given)\n");
-                 fprintf(stderr, "-o           Output the tree in DOT form to file 'tree-view.dot'\n");
-                 fprintf(stderr, "-r           Make the tree an RBT (the default is a BST)\n");
-                 fprintf(stderr, "\n");
-                 fprintf(stderr, "-h           Print this message\n");
-                 fprintf(stderr, "\n");
+                fprintf(stderr, "Usage: ./%s [OPTION]... <STDIN>\n", argv[0]);
+                fprintf(stderr, "\n");
+                fprintf(stderr, "Perform various operations using a binary tree.  By default, words\n");
+                fprintf(stderr, "are read from stdin and added to the tree, before being printed out\n");
+                fprintf(stderr, "alongside their frequencies to stdout.\n");
+                fprintf(stderr, "\n");
+                fprintf(stderr, "-c FILENAME  Check spelling of words in FILENAME using words\n");
+                fprintf(stderr, "             read from stdin as the dictionary.  Print timing\n");
+                fprintf(stderr, "             info & unknown words to stderr (ignore -d & -o)\n");
+                fprintf(stderr, "-d           Only print the tree depth (ignore -o)\n");
+                fprintf(stderr, "-f FILENAME  Write DOT output to FILENAME (if -o given)\n");
+                fprintf(stderr, "-o           Output the tree in DOT form to file 'tree-view.dot'\n");
+                fprintf(stderr, "-r           Make the tree an RBT (the default is a BST)\n");
+                fprintf(stderr, "\n");
+                fprintf(stderr, "-h           Print this message\n");
+                fprintf(stderr, "\n");
                 return(EXIT_SUCCESS);
                 break;
         }
     }
+    
     t = tree_new(tree_t);
-    /* timing info for fill */
+    
     fill_start = clock();
-
     while (getword(word, sizeof word, stdin) != EOF) {
         t = tree_insert(t, word);
     }
     fill_end = clock();
     fill_time = (fill_end - fill_start)/(double)CLOCKS_PER_SEC;
-    /* end timing info for fill */
-    if (inputFilename == NULL){
-        if (print_tree_depth == 1){
+    
+    if (inputFilename == NULL) {
+        if (print_tree_depth == 1) {
             printf("%d\n", tree_depth(t));
         }
         else if (output_dot_file == 1){
@@ -94,9 +117,7 @@ int main(int argc, char **argv) {
         else {
             tree_preorder(t, print_info);
         }
-    }
-    else {
-        /* timing info for search */
+    } else {
         search_start = clock();
         while (getword(word, sizeof word, document) != EOF) {
             if (tree_search(t, word) == 0){
@@ -107,14 +128,12 @@ int main(int argc, char **argv) {
         fclose(document);
         search_end = clock();
         search_time = (search_end - search_start)/(double)CLOCKS_PER_SEC;
-        /*end timing info for search */
             
-        /* printing info */
         fprintf(stderr, "Fill time     : %f\n", fill_time);
         fprintf(stderr, "Search time   : %f\n", search_time);
         fprintf(stderr, "Unknown words = %d\n", unknownWords);
-        /* end printing info */
     }
+    
     t = tree_free(t);
     return EXIT_SUCCESS;
 }
