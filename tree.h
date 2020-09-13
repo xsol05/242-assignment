074be128e6e5b69f7f698fb9d739df8009111483
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2020-09-13 18:46:35 +1200

    Edit syntax and comments

--- tree.h
+++ tree.h
@@ -1,11 +1,18 @@
+/*
+ * COSC242 Assignment 2020
+ * tree.h specification file
+ * Authors: Maaha Ahmad, Magdeline Huang, Kate Truman
+ */
+
 #ifndef TREE_H
 #define TREE_H
+
 #include <stddef.h>
 #include <stdio.h>
 
-
 typedef enum tree_e {BST, RBT} tree_t;
 typedef struct tree_node *tree;
+
 extern tree tree_free(tree t);
 extern void tree_inorder(tree t, void f(char *str));
 extern tree tree_insert(tree t, char *str, int isRoot);
@@ -15,4 +22,5 @@ extern int tree_search(tree t, char *str);
 extern int tree_depth(tree t);
 extern void tree_output_dot(tree t, FILE *out);
 extern void tree_colour_print(tree t);
-#endif /* tree_h */
+
+#endif
