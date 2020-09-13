4b3b4ca54d4e6a076c6e16e6c51b35b9a1f7422d
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2020-09-13 19:09:32 +1200

    Edit syntax and comments

--- mylib.h
+++ mylib.h
@@ -1,10 +1,19 @@
+/*
+ * COSC242 Assignment 2020
+ * Authors: Maaha Ahmad, Magdeline Huang, Kate Truman
+ */
+
 #ifndef MYLIB_H_
 #define MYLIB_H_
 
 #include <stddef.h>
 #include <stdio.h>
 
+/*
+ * Functions that are implemented in mylib.c
+ */
 extern void *emalloc(size_t);
 extern void *erealloc(void *, size_t);
 extern int getword(char *, int, FILE *);
+
 #endif
